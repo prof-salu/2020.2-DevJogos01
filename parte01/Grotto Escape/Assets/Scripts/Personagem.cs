66f78e175590b22aa3be61b41952d6bcd8f93b07
prof-salu <saluorodrigues@gmail.com>
2020-08-31 13:16:44 -0300

    parte01

---
+++ parte01/Grotto Escape/Assets/Scripts/Personagem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+    public float velMax = 5;
+    public float forcaPulo = 150;
+
+    private bool estaPulando = false;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        //Debug.Log("Apareceu Awake");
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();        
+    }
+
+    private void Start()
+    {
+        //Debug.Log("Apareceu Start");
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            estaPulando = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float movHorizontal = Input.GetAxis("Horizontal");
+        //Debug.Log(movHorizontal);
+        rb.velocity = new Vector2(movHorizontal * velMax, rb.velocity.y);
+
+        if((movHorizontal > 0 && sprite.flipX == true) || movHorizontal < 0 && sprite.flipX == false)
+        {
+            InverteSprite();
+        }
+        
+        if(estaPulando == true)
+        {
+            Pular();
+        }
+    }
+
+    private void Mover() { 
+        
+    }
+
+    private void InverteSprite()
+    {
+        sprite.flipX = !sprite.flipX;
+    }
+
+    private void Pular()
+    {
+        rb.AddForce(Vector2.up * forcaPulo);
+        estaPulando = false;
+    }
+}
